70e0c6e01084afbe577904b280146400fbffab05
John Blank <veloblank@gmail.com>
2020-01-13 19:54:32 -0500

    Adds delete button to remove from DOM

--- src/components/books/books.js
+++ src/components/books/books.js
@@ -21,19 +21,33 @@ class Books {
     }
     this.books.forEach(book => {
       let bookCard = `
-        <div class='book-card'>
-          <div class='book-cover'>
-            <img src='${book.img_src}'/>
-          </div>
-          <div class='book-info'>
-            <div class='book-title'>${book.title}</div>
-            <h4>${book.words} words</h4>
-            <p>${book.author}</p>
-            <p>Audiobook: ${book.string_reading_length}</p>
-          </div>
+      <div class='book-card'>
+        <button type='submit' class='delete-book' >
+          Delete
+        </button>
+        <div class='book-cover'>
+          <img src='${book.img_src}'/>
+        </div>
+        <div class='book-info'>
+          <div class='book-title'>${book.title}</div>
+          <h4>${book.words} words</h4>
+          <p>${book.author}</p>
+          <p>Audiobook: ${book.string_reading_length}</p>
+        </div>
         </div>
       `;
       booksContainer.innerHTML += bookCard;
+      this.addListeners();
+    });
+  }
+
+  addListeners() {
+    let booksContainer = document.getElementById("books-container");
+    let deleteButtons = document.querySelectorAll(".delete-book");
+    deleteButtons.forEach(button => {
+      button.addEventListener("click", () => {
+        return booksContainer.removeChild(button.parentElement);
+      });
     });
   }
 }
